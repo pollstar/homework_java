59771379631abe5a1a2992af9f0218b51a274ceb
pollstar <pollstar@ukr.net>
2022-02-15 17:51:33 +0200

    Complete task2 from hw9

---
+++ src/test/java/hw9/Task2/RandomTest.java
@@ -0,0 +1,7 @@
+package hw9.Task2;
+
+import java.util.Random;
+// Random wrapper class for Mockito
+class RandomTest extends Random {
+
+}
---
+++ src/test/java/ua/starovoitov/hw9/Task2/GetRandomNumberTest2.java
@@ -0,0 +1,7 @@
+package ua.starovoitov.hw9.Task2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetRandomNumberTest2 {
+
+}
