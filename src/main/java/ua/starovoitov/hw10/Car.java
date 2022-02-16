f5564b16d106b96cf7188e8053dd3b9dc9c3fe9c
pollstar <pollstar@ukr.net>
2022-02-16 16:28:55 +0200

    hw10 begin

---
+++ src/main/java/ua/starovoitov/hw10/Car.java
@@ -0,0 +1,15 @@
+package ua.starovoitov.hw10;
+
+public class Car {
+    private String series;
+    private int year;
+    private String color;
+    private int fuel;
+
+    public Car(String series, int year, String color, int fuel) {
+        this.series = series;
+        this.year = year;
+        this.color = color;
+        this.fuel = fuel;
+    }
+}
